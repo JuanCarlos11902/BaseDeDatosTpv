4cfacd3d07850c34d6a161f94724cf84c6d47a73
Juan Carlos Pérez Romero <105056958+JuanCarlos11902@users.noreply.github.com>
2024-06-16 21:37:31 +0200

    terminado

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const dbURI = process.env.MONGODB_URI || "mongodb+srv://jcperezromero02:01p0h7b19dCK3qT0@rinconloscuatrobasededa.lmgnbfz.mongodb.net/?retryWrites=true&w=majority&appName=RinconLosCuatroBaseDeDatos";
+const dbURI = process.env.MONGODB_URI || "mongodb+srv://jcperezromero02:01p0h7b19dCK3qT0@cluster0.ppamsvx.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(dbURI);
+
+module.exports = mongoose;
